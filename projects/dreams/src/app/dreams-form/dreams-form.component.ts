3744dfd2b2b8e0c9e63ab53d80d80d43c1bb7763
sbarranco <sbarrancovico@gmail.com>
2025-03-05 23:37:57 +0100

    feat(dreams): enhance dreams form and list components with improved UI and functionality; implement reactive form for dream submission and display posted dreams

--- projects/dreams/src/app/dreams-form/dreams-form.component.ts
+++ projects/dreams/src/app/dreams-form/dreams-form.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { DreamsService } from '../dreams-service/dreams.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dreams-form',
   templateUrl: './dreams-form.component.html',
   styleUrl: './dreams-form.component.scss',
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class DreamsformComponent {
   title = 'dreams form';
+
+  private dreamsService = inject(DreamsService);
+
+  form: FormGroup = new FormGroup({
+    description: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(1000),
+    ]),
+  });
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      this.dreamsService.addDream(this.form.value);
+      this.form.reset();
+    }
+  }
 }
--- projects/dreams/src/app/dreams-list/dreams-list.component.ts
+++ projects/dreams/src/app/dreams-list/dreams-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostBinding, inject, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { DreamsService } from '../dreams-service/dreams.service';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dreams-list',
   templateUrl: './dreams-list.component.html',
   styleUrl: './dreams-list.component.scss',
+  imports: [CommonModule, MatCardModule, MatButtonModule, RouterModule],
 })
-export class DreamsListComponent {
+export class DreamsListComponent implements OnInit {
   title = 'dreams list';
+  @HostBinding('class') class = 'app-dreams-list';
+
+  private dreamsService = inject(DreamsService);
+
+  dreams$ = this.dreamsService.dreams$;
+
+  ngOnInit(): void {
+    this.dreamsService.getDreams();
+  }
 }
