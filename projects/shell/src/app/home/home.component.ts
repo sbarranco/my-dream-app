2005a0ae317123a767be03960d71b5ff5cde26d5
sbarranco <sbarrancovico@gmail.com>
2025-03-05 23:36:58 +0100

    feat(home): enhance home component with dynamic card display and styling; refactor template and add routing functionality

--- projects/shell/src/app/home/home.component.ts
+++ projects/shell/src/app/home/home.component.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, signal } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostBinding, OnInit, signal, inject } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CardComponent } from '../card/card.component';
+import { CARDS } from '../card/card-data';
+import { Card } from '../card/card.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, CardComponent],
 })
 export class HomeComponent implements OnInit {
+  @HostBinding('class') class = 'app-home';
+  private readonly router = inject(Router);
+
   title = 'shell';
 
   user = signal<string | null>(null);
 
+  cards: Card[] = CARDS;
+
   ngOnInit(): void {
     this.user.update(() => localStorage.getItem('user'));
+  }
+
+  onClickCardButton(path: string | undefined): void {
+    this.router.navigate([path]);
   }
 }
